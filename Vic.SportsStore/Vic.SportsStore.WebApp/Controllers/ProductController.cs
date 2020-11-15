258330a106244f415e3fc50f7a39a491f13ee967
shendu <shendu@gmail.com>
2020-11-15 16:59:51 -0500

    1.[new] add new package

---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.Domain.Entities;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IProductsRepository ProductsRepository { get; set; }
+          = new InMemoryProductRepository();
+        public ViewResult List()
+        {
+            return View(ProductsRepository.Products);
+        }
+    }
+}
