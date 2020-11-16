1e33ae23a9c5cad155d509d6be3e6024b2b2e6cd
shendu <shendu@gmail.com>
2020-11-15 22:14:56 -0500

    1.[new] add paging

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Vic.SportsStore.Domain.Abstract;
 using Vic.SportsStore.Domain.Concrete;
-using Vic.SportsStore.Domain.Entities;
+using Vic.SportsStore.WebApp.Models;
 
 namespace Vic.SportsStore.WebApp.Controllers
 {
@@ -13,9 +13,33 @@ namespace Vic.SportsStore.WebApp.Controllers
     {
         public IProductsRepository ProductsRepository { get; set; }
           = new EFProductRepository();
-        public ViewResult List()
+
+        public int PageSize = 3;
+
+        public ViewResult List(int page = 1)
         {
-            return View(ProductsRepository.Products);
+            //var model = ProductsRepository
+            //.Products
+            //.OrderBy(p => p.ProductId)
+            //.Skip((page - 1) * PageSize)
+            //.Take(PageSize);
+
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = ProductsRepository
+                .Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = ProductsRepository.Products.Count()
+                }
+            };
+
+            return View(model);
         }
     }
 }
