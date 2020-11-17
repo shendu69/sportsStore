b0a95f2f97aed58b9c5c61297dd9a078d03e434d
shendu <shendu@gmail.com>
2020-11-17 00:01:26 -0500

    1.[new] add new projects

---
+++ Shen.Sportchek/Shen.Sportchek.DebugConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shen.Sportchek.DebugConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
