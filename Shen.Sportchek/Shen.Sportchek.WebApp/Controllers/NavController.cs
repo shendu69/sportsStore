c41aa4f193ff5531e790353aa5014e4a0c41d1db
shendu <shendu@gmail.com>
2020-11-22 16:17:59 -0500

    1.[CHG] add new menu

---
+++ Shen.Sportchek/Shen.Sportchek.WebApp/Controllers/NavController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shen.Sportchek.Domain.Abstract;
+using Shen.Sportchek.Domain.Concrete;
+
+namespace Shen.Sportchek.WebApp.Controllers
+{
+    public class NavController : Controller
+    {
+        // GET: Nav
+        public IProductsRepository ProductsRepository { get; set; }
+          = new EFProductRepository();
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = ProductsRepository
+            .Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x);
+
+            return PartialView(categories);
+        }
+    }
+}
